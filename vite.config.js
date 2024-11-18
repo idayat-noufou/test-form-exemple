f54d5d52a0569580c165ed51d68fc31cd287fd6e
Idayat Noufou <idayat.noufou@ynov.com>
2024-11-18 15:03:56 +0100

    correction codecov config avec vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/test-form-exemple/"
+  plugins: [
+      react(),
+      codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "test-form-exemple",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
+  base: "/test-form-exemple/",
 })
